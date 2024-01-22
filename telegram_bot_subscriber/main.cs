32143bf30f89630dcbda1ac49351b5d4050630f5
Abdulaziz Egamberdiyev <145344817+abdulaziz0413@users.noreply.github.com>
2024-01-22 11:07:09 +0500

    keraksiz usinglar tozlandi

--- telegram_bot_subscriber/main.cs
+++ telegram_bot_subscriber/main.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BotTestTelegram;
-
-namespace telegram_bot_subscriber
+﻿namespace telegram_bot_subscriber
 {
     internal class main
     {
